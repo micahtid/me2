30ed96bf1342cd0aa30235e9fd6d497a96a11eff
Micah Tidball <micahtid@gmail.com>
2024-10-24 22:10:20 +0700

    added tooltips (for room buttons)

--- app/components/RoomsSection/RoomsDisplay.tsx
+++ app/components/RoomsSection/RoomsDisplay.tsx
@@ -8,7 +8,25 @@ import { roomTags } from "@/app/data";
 import { FaLink } from "react-icons/fa";
 import { IoEnter, IoExit } from "react-icons/io5";
 import { MdDelete, MdModeEdit, MdStart } from "react-icons/md";
+
 import RoomUserList from "./RoomUserList";
+import { twMerge } from "tailwind-merge";
+
+interface ToolTipProps {
+  message: string;
+  className?: string;
+}
+
+const ToolTip: React.FC<ToolTipProps> = ({ message, className }) => {
+  return (
+    <div className={twMerge(`
+    absolute left-1/2 transform -translate-x-1/2 -top-[40px]
+    bg-black/80 text-nowrap py-1 px-2 rounded-lg
+    transition-all duration-150`, className)}>
+      {message}
+    </div>
+  )
+}
 
 const iconStyles = `p-2 bg-black/80 rounded-full text-white`;
 
@@ -43,8 +61,22 @@ const RoomButton: React.FC<RoomButtonProps> = ({ room, user, disabled }) => {
     }
   };
 
+  const getToolTipMessage = () => {
+    if (room.roomId === user?.uid) {
+      return "Delete Room";
+    } else if (room.users.includes(user?.uid)) {
+      return "Leave Room";
+    } else {
+      return "Join Room";
+    }
+  }
+
   return (
-    <button onClick={handleButtonClick} className={`${iconStyles} ${disabled && 'opacity-50 cursor-not-allowed'}`} disabled={disabled}>
+    <button 
+    onClick={handleButtonClick} 
+    className={twMerge(iconStyles, 'relative group')} 
+    disabled={disabled}>
+      <ToolTip message={getToolTipMessage()} className="opacity-0 group-hover:opacity-100" />
       {renderIcon()}
     </button>
   );
@@ -83,7 +115,8 @@ const RoomsDisplay = () => {
             (room.roomId === user?.uid || room.users.includes(user?.uid)) && "bg-primary/30"
           }`}
         >
-          <div className="flex justify-between w-full items-start gap-x-4 px-8 pt-6">
+
+          <div className="flex justify-between w-full items-start gap-x-4 pl-8 pr-16 pt-6">
             <div>
               <h3 className="text-xl font-semibold">{room.description}</h3>
               <p className="text-gray-400 mb-2">Created {getHoursAgo(room.createdAt)} hours ago</p>
@@ -91,32 +124,41 @@ const RoomsDisplay = () => {
                 <RoomUserList display={`${room.users.length}/${room.limit} Users`} users={users} roomUserIds={room.users} />
               </div>
             </div>
+
             <div className="flex justify-center items-center gap-x-2">
+
               {room.roomId === user?.uid && (
-                <a target="_blank" href={room.startUrl} className={iconStyles}>
+                <a target="_blank" href={room.startUrl} className={twMerge(iconStyles, 'relative group')}>
+                  <ToolTip message="Start Room" className="opacity-0 group-hover:opacity-100" />
                   <MdStart />
                 </a>
               )}
+
               {room.users.includes(user?.uid) && room.roomId !== user?.uid && (
-                <a target="_blank" href={room.joinUrl} className={iconStyles}>
+                <a target="_blank" href={room.joinUrl} className={twMerge(iconStyles, 'relative group')}>
+                  <ToolTip message="Join Room" className="opacity-0 group-hover:opacity-100" />
                   <FaLink />
                 </a>
               )}
+
               {room.roomId === user?.uid && (
                 <button
-                  className={iconStyles}
+                  className={twMerge(iconStyles, 'relative group')}
                   onClick={() => {
                     setActiveRoom(room);
                     setIsNewRoom(false);
                     onModalOpen();
                   }}
                 >
+                  <ToolTip message="Edit Room" className="opacity-0 group-hover:opacity-100" />
                   <MdModeEdit />
                 </button>
               )}
+
               <RoomButton room={room} user={user} disabled={userInAnyRoom && !(room.roomId === user?.uid || room.users.includes(user?.uid))} />
             </div>
           </div>
+
           <div className="flex flex-row justify-start items-center gap-x-2 bg-secondary px-8 py-2">
             {room.tags.map((tag: string, index: number) => (
               <div key={index} className="font-medium bg-white/50 px-2 py-1 rounded-lg">
